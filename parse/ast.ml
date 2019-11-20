2d49d9fc2e33a8c7365fd02c955e94511766a0ea
aizjForever <aizejie2014@163.com>
2019-11-20 14:55:48 -0500

    Added the product type

--- parse/ast.ml
+++ parse/ast.ml
@@ -10,6 +10,7 @@ type typ =
 | ANYLIST (*for typing nil*)
 | LIST of annotated_typ
 | Arrow of annotated_typ * annotated_typ
+| Prod of typ * typ 
 
 
 and annotated_typ = typ * Int32.t
@@ -27,6 +28,8 @@ type exp =
 | App of exp * exp
 | Let of var_annot * exp * exp (*let x/_ = e1 in e2*)
 | Match of exp * exp * var_annot * var_annot * exp (*match e with | [] -> e1 | x/_ :: xs/_ -> e2*)
+| Pair of exp * exp
+| Letp of exp * var_annot * var_annot * exp (*letp x1,x2 = e in e1*)
 | Triv
 | NIL of typ option
 
@@ -43,7 +46,8 @@ let rec sub_type ty1 ty2 = match (ty1, ty2) with |
 | Arrow (a,b), Arrow (a',b') -> sub_type_annot a' a && sub_type_annot b b'
 | ANYLIST, ANYLIST -> true
 | ANYLIST, LIST _ -> true
-| LIST _, ANYLIST -> false 
+| LIST _, ANYLIST -> false
+| Prod (a,b), Prod (c,d) -> sub_type a c && sub_type b d
 | _ -> false 
 
 and sub_type_annot aty1 aty2 = match (aty1, aty2) with |
@@ -53,8 +57,9 @@ and sub_type_annot aty1 aty2 = match (aty1, aty2) with |
 let rec typ_to_string = function
 | UNIT -> "unit"
 | ANYLIST -> "L(Any)"
-| LIST typ -> "L" ^ (annotated_typ_to_string typ)
+| LIST typ -> sprintf "L(%s)" (annotated_typ_to_string typ)
 | Arrow (t1,t2) -> (annotated_typ_to_string t1) ^ " -> " ^ (annotated_typ_to_string t2)
+| Prod (t1, t2) -> sprintf "(%s * %s)" (typ_to_string t1) (typ_to_string t2) 
 and annotated_typ_to_string =
 function | (typ, pot) -> sprintf "<%s,%ld>" (typ_to_string typ) pot
 
@@ -71,26 +76,30 @@ let rec exp_to_string = function
 | App (e1,e2) -> sprintf "(%s %s)" (exp_to_string e1) (exp_to_string e2)
 | Let (WILD, e, e1) -> sprintf "let _ = %s \n in %s \n end" (exp_to_string e) (exp_to_string e1)
 | Let (ANNOT (id, None), e, e1) ->
-  sprintf "let %s = %s \n in %s \n end" (Symbol.name id) (exp_to_string e) (exp_to_string e1)
+  sprintf "(let %s = %s \n in %s \n end)" (Symbol.name id) (exp_to_string e) (exp_to_string e1)
 | Let (ANNOT (id, Some typ), e, e1) ->
-  sprintf "let %s: %s = %s \n in %s \n end" (Symbol.name id) (typ_to_string typ) (exp_to_string e) (exp_to_string e1)
+  sprintf "(let %s: %s = %s \n in %s \n end)" (Symbol.name id) (typ_to_string typ) (exp_to_string e) (exp_to_string e1)
 
 | Match (e,e1,idx,idxs,e2) ->
-  sprintf "match (%s) with \n | [] -> %s \n | %s::%s -> %s" (exp_to_string e) 
+  sprintf "(match (%s) with \n | [] -> %s \n | %s::%s -> %s)" (exp_to_string e) 
   (exp_to_string e1) (make_id_from_typ_annot idx) (make_id_from_typ_annot idxs) (exp_to_string e2)
 | Triv -> "()"
 | NIL None -> "[]"
 | NIL (Some ty) -> sprintf "([]: %s)" (typ_to_string ty)
+| Pair (e1, e2) -> sprintf "(%s, %s)" (exp_to_string e1) (exp_to_string e2)
+| Letp (e, idx1, idx2, e1) -> 
+    sprintf "letp %s, %s = %s\n in\n %s \n end" (make_id_from_typ_annot idx1) 
+    (make_id_from_typ_annot idx2) (exp_to_string e) (exp_to_string e1)
+
 | _ -> failwith "Impossible"
 
 
 
-let args_to_string =
-  List.fold_left (fun acc (typ, ident)-> acc ^ (sprintf "%s:%s, " (Symbol.name ident) (annotated_typ_to_string typ))) ""
+let args_to_string (typ, id) = sprintf "%s: %s" (Symbol.name id) (annotated_typ_to_string typ)
 
 let rec gdecl_to_string = function
 | Fdefn (name, args, ret_typ, body)->
-  sprintf "fun %s (%s): %s = \n  %s" (Symbol.name name) (args_to_string [args]) (annotated_typ_to_string ret_typ) (exp_to_string body)
+  sprintf "fun %s (%s): %s = \n  %s" (Symbol.name name) (args_to_string args) (annotated_typ_to_string ret_typ) (exp_to_string body)
 
 
-let rec to_string = List.fold_left (fun base decl -> base ^ (gdecl_to_string decl)) ""
+let rec to_string = List.fold_left (fun base decl -> base ^ (gdecl_to_string decl) ^ "\n") ""
